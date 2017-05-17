2bf1f4c67c49d6fdcdd77f5d22dcb956a2ed4a3b
Loertz <watte.william@gmail.com>
2017-05-17 08:00:19 +0200

    eval ha8.3

--- chat.py
+++ chat.py
@@ -90,7 +90,7 @@ class ChatBackend(object):
     def update(self):
 
         activity_data = {
-            {
+            string(i): {
                 "name": "Johnny",
                 "n": i,
                 "lastEvent": "ABSENCE",
