ba9f889aba73e6283d2794374c696923f6050572
Loertz <watte.william@gmail.com>
2017-05-23 17:43:30 +0200

    add top header modif

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -1,10 +1,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
-    document.getElementById("main").style.marginLeft = "100%";
+/*    document.getElementById("main").style.marginLeft = "100%";*/
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("main").style.marginLeft = "20%";
+    document.getElementById("main").style.marginRight= "20%";
 }
