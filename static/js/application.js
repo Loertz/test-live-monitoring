dd38f2efd6f3a61e0bd7d1a6952c723403f1f17b
Loertz <watte.william@gmail.com>
2017-05-23 10:10:43 +0200

    parse ?

--- static/js/application.js
+++ static/js/application.js
@@ -10,7 +10,7 @@ var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
 var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
 
 inbox.onmessage = function(message) {
-  console.log(message.data);
+  console.log(JSON.parse(message.data);
   var data = JSON.parse(message.data);
   if (!initial) {
       data.forEach(function create(val) {
@@ -96,7 +96,7 @@ inbox.onmessage = function(message) {
 };
 
 
-
+console.log('update');
   /*
   $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
   $("#chat-text").stop().animate({
