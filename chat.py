26a39ffcf7e161e2a6ae5f47085ca78fef2be387
Loertz <watte.william@gmail.com>
2017-05-23 09:59:36 +0200

    outbox ?

--- chat.py
+++ chat.py
@@ -157,6 +157,18 @@ chats.start()
 def hello():
     return render_template('index.html')
 
+@sockets.route('/submit')
+def inbox(ws):
+    """Receives incoming chat messages, inserts them into Redis."""
+    while not ws.closed:
+        # Sleep to prevent *contstant* context-switches.
+        gevent.sleep(0.1)
+        message = ws.receive()
+
+        if message:
+            app.logger.info(u'Inserting message: {}'.format(message))
+            redis.publish(REDIS_CHAN, message)
+
 
 @sockets.route('/receive')
 def outbox(ws):
