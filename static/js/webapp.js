13e19b5c9be9644b88159ca541f69e994061ef7c
Loertz <watte.william@gmail.com>
2017-05-24 00:32:36 +0200

    2 grlt print iter (?) modifs /n saut de ligne

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -18,16 +18,16 @@ function initiate (data) {
     node.id = "div"+val.n;
 
     var resd = document.createElement('h5');
-    resd.innerHTML = val.name +"\n" + "Chambre :  n°" + val.n;
+    resd.innerText = val.name +\n + "Chambre :  n°" + val.n;
 
     var dur = document.createElement('h6');
 
-    dur.innerHTML = val.tmc
+    dur.innerText = val.tmc;
     dur.id = val.n;
 
     node.appendChild(resd);
     node.appendChild(dur);
-
+    node.style.display="inline";
     monitoring.appendChild(node);
 
   });
@@ -38,40 +38,40 @@ function update_css(data)
 {data.forEach(function creat( val) {
       if (val.acti == "") {
 
-          document.getElementById(val.n).innerHTML = val.tmc
+          document.getElementById(val.n).innerText = val.tmc;
           document.getElementById("div"+val.n).style.display = "none";
           /*document.getElementById("div"+val.n).style.backgroundColor = "rgb(63, 63, 78)"*/
       }
 
       else if (val.acti =="1") {
-          document.getElementById(val.n).innerHTML = val.tmc
+          document.getElementById(val.n).innerText = val.tmc;
             /*bleu :#1D7FB2; vert : #8C8910; rouge : #CA1725; gris :rgb(63, 63, 78);*/
             if (val.lastEvent == "BEDROOM") {
-              document.getElementById("div"+val.n).style.backgroundColor = "#8C8910"
+              document.getElementById("div"+val.n).style.backgroundColor = "#8C8910";
                }
             else if (val.lastEvent == "BATHROOM") {
-              document.getElementById("div"+val.n).style.backgroundColor ="#1D7FB2"
+              document.getElementById("div"+val.n).style.backgroundColor ="#1D7FB2";
             }
             else if (val.lastEvent == "FALL") {
-              document.getElementById("div"+val.n).style.backgroundColor = "#CA1725"
+              document.getElementById("div"+val.n).style.backgroundColor = "#CA1725";
             }
             else {
               /*document.getElementById("div"+val.n).style.backgroundColor = "rgb(63, 63, 78)"*/
             }
-            document.getElementById("div"+val.n).style.display = "block";
+            document.getElementById("div"+val.n).style.display = "show";
         }
 
       else {
-          document.getElementById(val.n).innerHTML = val.tmc
+          document.getElementById(val.n).innerText = val.tmc;
             /*bleu :#1D7FB2; vert : #8C8910; rouge : #CA1725; gris :rgb(63, 63, 78);*/
             if (val.lastEvent == "BEDROOM") {
-              document.getElementById("div"+val.n).style.backgroundColor = "#8C8910"
+              document.getElementById("div"+val.n).style.backgroundColor = "#8C8910";
             }
             else if (val.lastEvent == "BATHROOM") {
-              document.getElementById("div"+val.n).style.backgroundColor = "#1D7FB2"
+              document.getElementById("div"+val.n).style.backgroundColor = "#1D7FB2";
             }
             else if (val.lastEvent == "FALL") {
-              document.getElementById("div"+val.n).style.backgroundColor = "#CA1725"
+              document.getElementById("div"+val.n).style.backgroundColor = "#CA1725";
             }
             else {
               /*document.getElementById("div"+val.n).style.backgroundColor = "rgb(63, 63, 78)"*/
