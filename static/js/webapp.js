025734114c3fb1bc892e666a72e6f10fbf4ca9dc
Loertz <watte.william@gmail.com>
2017-05-23 19:45:43 +0200

    bootstrap, css button

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -6,5 +6,5 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("main").style.marginRight= "20%";
+/*    document.getElementById("main").style.marginRight= "20%";*/
 }
