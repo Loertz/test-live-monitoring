77f78b32a91fae9b80b13ac28e5ce7410153c806
Loertz <watte.william@gmail.com>
2017-05-23 14:51:33 +0200

    ajout de la barre a droite

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -1,10 +1,10 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
 }
