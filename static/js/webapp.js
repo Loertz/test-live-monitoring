18d4f7481cb6481256c792d8fb9b678f4a0bbaf7
Loertz <watte.william@gmail.com>
2017-05-24 19:43:49 +0200

    ? web app

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -1,15 +1,15 @@
-$.("#journal").click(function openNav() {
-    $.("#mySidenav").style.width = "100%";
+$("#journal").click(function openNav() {
+    $("#mySidenav").style.width = "100%";
     var on = true;
 /*    document.getElementById("main").style.marginLeft = "100%";*/
 });
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-$.("#journal").click(function closeNav() {
+$("#journal").click(function closeNav() {
   if (on) {
-      $.("#mySidenav").style.width = "20%";
+      $("#mySidenav").style.width = "20%";
       var on = false:
-  /*    $.("#main").style.marginRight= "20%"; */
+  /*    $("#main").style.marginRight= "20%"; */
   };
 });
 
@@ -41,27 +41,27 @@ function update_css(data)
 {data.forEach(function creat( val) {
       if (val.acti == "") {
 
-          $.(val.n).innerText = val.tmc;
-          $.("div"+val.n).style.display = "none";
-          /*$.("#div"+val.n).style.backgroundColor = "#f2f2f2"*/
+          $(val.n).innerText = val.tmc;
+          $("div"+val.n).style.display = "none";
+          /*$("#div"+val.n).style.backgroundColor = "#f2f2f2"*/
       }
 
       else {
-        $.(val.n).innerText = val.tmc;
+        $(val.n).innerText = val.tmc;
             /*bleu :#1D7FB2; vert : #8C8910; rouge : #CA1725; gris :#f2f2f2;*/
             if (val.lastEvent == "BEDROOM") {
-              $.("#div"+val.n).style.backgroundColor = #f2f2f2;
+              $("#div"+val.n).style.backgroundColor = #f2f2f2;
             }
             else if (val.lastEvent == "BATHROOM") {
-              $.("#"+"div"+string(val.n)).style.backgroundColor = "#1D7FB2";
+              $("#"+"div"+string(val.n)).style.backgroundColor = "#1D7FB2";
             }
             else if (val.lastEvent == "FALL") {
-              $.("#div"+val.n).style.backgroundColor = "#CA1725";
+              $("#div"+val.n).style.backgroundColor = "#CA1725";
             }
             else {
-              /*$.("#div"+val.n).style.backgroundColor = "#f2f2f2"*/
+              /*$("#div"+val.n).style.backgroundColor = "#f2f2f2"*/
             };
-        $.("#div"+val.n).style.display = "show";
+        $("#div"+val.n).style.display = "show";
       };
   });
 };
