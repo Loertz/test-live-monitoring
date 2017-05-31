b9015b4bc4bdeedee206fb87bc78ab7ef4410481
Loertz <watte.william@gmail.com>
2017-05-31 15:46:35 +0200

    ?

--- chat.py
+++ chat.py
@@ -118,7 +118,7 @@ class LiveMonitoringBackend(object):
         # Fais la requetes des données et les stocke sous
         # answer = (reponse1,reponse2,...,response n)
         try : answer = grequests.map(rs)
-    except : print('failed to load')
+        except : print('failed to load')
         data = json.loads(redis.get('activity_data'))
         # print(answer)
         i = 0
